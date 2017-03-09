96aa73c45682892098cc9765abd5f6c62e0c708a
Courtney Labog <labogcourtney@gmail.com>
2017-03-09 14:33:43 -0500

    updated index

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,8 +5,8 @@ zomatoApp = {};
 zomatoApp.inputLocationName = "";
 zomatoApp.inputLatitude = "";
 zomatoApp.inputLongitude = "";
-zomatoApp.restaurantsNearby = [];
 zomatoApp.allRestaurants = [];
+zomatoApp.restaurantsNearby = [];
 
 //helper function that displays variables
 zomatoApp.displayResults = function (a, b, c) {
@@ -30,13 +30,11 @@ zomatoApp.getUserLocation = function () {
         zomatoApp.displayResults(zomatoApp.inputLocationName, zomatoApp.inputLatitude, zomatoApp.inputLongitude); // this can be removed...it was just displaying the info
         zomatoApp.getRestaurants(zomatoApp.inputLatitude, zomatoApp.inputLongitude);
     });
-
     google.maps.event.addDomListener(input, 'keydown', function (e) {
         if (e.keyCode == 13) {
             e.preventDefault();
         }
     });
-
     console.log('get user location printed');
 };
 
@@ -93,36 +91,27 @@ zomatoApp.getRestaurants = function (latitude, longitude) {
         var allRestaurantsTwo = dataTwo[0].restaurants;
         var allRestaurantsThree = dataThree[0].restaurants;
         var allRestaurantsResults = allRestaurantsOne.concat(allRestaurantsTwo, allRestaurantsThree);
-        zomatoApp.allRestaurants.push(allRestaurantsResults);
+        zomatoApp.allRestaurants.push(allRestaurantsResults); //allRestaurantsResults is a global var
+
         console.log(zomatoApp.allRestaurants);
 
-        zomatoApp.allRestaurants[0].forEach(function (item) {
-            console.log(item);
+        var results = allRestaurantsResults;
+
+        var userChoiceRating = "3"; //needs to be changed for user input rating
+        var filteredByRating = results.forEach(function (item) {
+            var restaurantRating = item.restaurant.user_rating.aggregate_rating;
+            var restaurantName = item.restaurant.name;
+            if (restaurantRating >= userChoiceRating) {
+                console.log(restaurantName, restaurantRating);
+                //storing filtered results
+                zomatoApp.restaurantsNearby.push(item);
+            } else {
+                console.log("we broken");
+            }
         });
+        console.log(zomatoApp.inputLatitude, zomatoApp.inputLongitude);
+        // console.log(zomatoApp.restaurantsNearby);
     });
-
-    //whatthefaaaa
-    // var results = zomatoApp.allRestaurants.restaurants; 
-
-    //     console.log(results);
-    // var userChoiceRating = "3"; //needs to be changed for user input rating
-    // var filteredByRating = results.forEach(function(item){
-    //     var restaurantRating = item.restaurant.user_rating.aggregate_rating;
-    //     var restaurantName = item.restaurant.name;
-    //     if (restaurantRating >= userChoiceRating){
-    //         console.log(restaurantName, restaurantRating);
-    //         //storing filtered results
-    //         zomatoApp.restaurantsNearby.push(item);
-    //     } else {
-    //         console.log("we broken");
-    //     }
-    // });
-    // console.log(zomatoApp.inputLatitude, zomatoApp.inputLongitude);
-    // console.log(zomatoApp.restaurantsNearby);
-
-    // });
-    // }
-
 };
 
 zomatoApp.init = function () {
